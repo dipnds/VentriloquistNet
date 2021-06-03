45c71f3a11162f8ec93b2df8d7203cf8944abc84
Deepan Das <dasd@atcremers10.in.tum.de>
2021-06-03 14:23:31 +0200

    failed run of mover and full run of emo

--- mover/networks/emo_classifier_seq.py
+++ mover/networks/emo_classifier_seq.py
@@ -5,27 +5,31 @@ class Net(nn.Module):
     def __init__(self):
         super(Net,self).__init__()
         
-        # 128, 92 (f, t=1s)
+        # 30, 92 (f, t=1s)
         self.feature = nn.Sequential(
             nn.Conv2d(1, 32, (3,7), padding=(1,3)),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
-            nn.AvgPool2d((4,1)),
+            nn.AvgPool2d((2,1)), #15,92
+            #nn.Dropout(p=0.2),
             nn.Conv2d(32, 32, (3,5), padding=(1,2)),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
-            nn.AvgPool2d((4,1)),
+            nn.AvgPool2d((2,1), padding=(1,0)), #8,92
+            #nn.Dropout(p=0.2),
             nn.Conv2d(32, 64, 3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.AvgPool2d((4,1)),
+            nn.AvgPool2d((2,1)), #4,92
+            # nn.Dropout(p=0.2),
             nn.Conv2d(64, 128, 3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d((2,3))
+            nn.MaxPool2d((4,3)) #1,30
             )
         self.classifier = nn.Sequential(
             nn.Flatten(),
+            nn.Dropout(p=0.2),
             nn.Linear(128*30, 512),
             nn.Dropout(p=0.2),
             nn.BatchNorm1d(512),
