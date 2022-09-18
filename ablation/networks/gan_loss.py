dcb1d3d6614e63266a24237561970a9a4264a6e1
dipnds <dipn.ds@gmail.com>
2022-09-18 12:00:27 +0200

    cleanup 1 for testing branch

--- ablation/networks/gan_loss.py
+++
@@ -1,5 +0,0 @@
-import torch.nn as nn
-import torch
-
-def lipsync_cossim():
-    
--- ablation/temp.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import torch
-
-mean_kp = torch.load('kp_general.pt').flatten().unsqueeze(0).unsqueeze(0)
-wt = torch.zeros(136); wt[-40:] = 1; wt[:34] = 1
-wt = torch.diag(wt)
-mean_kp = torch.matmul(mean_kp,wt)
-mean_kp = mean_kp.reshape((-1,2))
-plt.figure(); plt.scatter(mean_kp[:,0],-mean_kp[:,1],s=2); plt.savefig('meankp.png')
